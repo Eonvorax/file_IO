99bcfcaf0bd6f47868a23fdf084b751f82e7c0f1
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-08-04 19:58:16 +0000

    Fix: betty style

--- main_rot.c
+++ main_rot.c
@@ -1,9 +1,14 @@
 #include "main.h"
-
-int main ()
+/**
+ * main - for testing purposes
+ *
+ * Return: 0
+*/
+int main(void)
 {
-    int n_rot = 50;
-    char c = 'a';
-    printf("ROT%d(%c) : %c\n", n_rot, c, rot_char_n(c, n_rot));
-    return (0);
+	int n_rot = 26;
+	char c = 'a';
+
+	printf("ROT%d(%c) : %c\n", n_rot, c, rot_char_n(c, n_rot));
+	return (0);
 }
--- rot.c
+++ rot.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
+/**
+ * rot_char_n -
+ */
 char rot_char_n(char c, int n)
 {
-    if (c >= 'A' && c <= 'Z')
-    {
-        return (((c - 'A') + n) % 26 + 'A');
-    }
-    else if (c >= 'a' && c <= 'z')
-    {
-        return (((c - 'a') + n) % 26 + 'a');
-    }
-    return (0);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (((c - 'A') + n) % 26 + 'A');
+	}
+	if (c >= 'a' && c <= 'z')
+	{
+		return (((c - 'a') + n) % 26 + 'a');
+	}
+	return (0);
 }
